a5c3f0bfb9b683a4c47f604c545d4606a58b64b7
Keith Mok <kmok@cyngn.com>
2015-11-13 09:46:14 -0800

    power: Refactor TARGET_TAP_TO_WAKE_NODE
    Add TARGET_POWER_SET_FEATURE_LIB, since other devices
    are not simply writing a 0/1 to a device node.
    TARGET_TAP_TO_WAKE_NODE is still valid and both
    TARGET_TAP_TO_WAKE_NODE and TARGET_POWER_SET_FEATURE_LIB are
    allowed to coexist. (Currently set_feature only has double tap
    to wake, but it may be extended in future)
    
    Change-Id: I5c6204fd38a6fbe48e8aac49843bfa047c4b6436

--- Android.mk
+++ Android.mk
@@ -72,6 +72,10 @@ ifeq ($(call is-board-platform-in-list,msmnile), true)
 LOCAL_SRC_FILES += power-msmnile.c
 endif
 
+ifneq ($(TARGET_POWER_SET_FEATURE_LIB),)
+    LOCAL_STATIC_LIBRARIES += $(TARGET_POWER_SET_FEATURE_LIB)
+endif
+
 ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
     LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
 endif
