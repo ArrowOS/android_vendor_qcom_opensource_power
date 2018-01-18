85b67d80c4668931756ffd16e0fe58d2d0a62f0f
dianlujitao <dianlujitao@lineageos.org>
2018-01-18 22:05:14 +0800

    power: Remove mutex to hints
     * No longer needed.
    
    Change-Id: I09fc90b4e5399b3f3b8a409edf3f57361da90100

--- power-660.c
+++ power-660.c
@@ -36,7 +36,6 @@
 #include <fcntl.h>
 #include <dlfcn.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <unistd.h>
 
 #define LOG_TAG "QTI PowerHAL"
@@ -57,7 +56,6 @@ static int display_hint_sent;
 static int video_encode_hint_sent;
 static int cam_preview_hint_sent;
 
-pthread_mutex_t camera_hint_mutex = PTHREAD_MUTEX_INITIALIZER;
 static int camera_hint_ref_count;
 static void process_video_encode_hint(void *metadata);
 //static void process_cam_preview_hint(void *metadata);
@@ -262,7 +260,6 @@ static void process_video_encode_hint(void *metadata)
                 memcpy(resource_values, res, MIN_VAL(sizeof(resource_values), sizeof(res)));
                 num_resources = sizeof(res)/sizeof(res[0]);
             }
-            pthread_mutex_lock(&camera_hint_mutex);
             camera_hint_ref_count++;
             if (camera_hint_ref_count == 1) {
                 if (!video_encode_hint_sent) {
@@ -271,19 +268,16 @@ static void process_video_encode_hint(void *metadata)
                     video_encode_hint_sent = 1;
                 }
            }
-           pthread_mutex_unlock(&camera_hint_mutex);
         }
     } else if (video_encode_metadata.state == 0) {
         if ((strncmp(governor, INTERACTIVE_GOVERNOR,
             strlen(INTERACTIVE_GOVERNOR)) == 0) &&
             (strlen(governor) == strlen(INTERACTIVE_GOVERNOR))) {
-            pthread_mutex_lock(&camera_hint_mutex);
             camera_hint_ref_count--;
             if (!camera_hint_ref_count) {
                 undo_hint_action(video_encode_metadata.hint_id);
                 video_encode_hint_sent = 0;
             }
-            pthread_mutex_unlock(&camera_hint_mutex);
             return ;
         }
     }
