8a79d0d1fddc2cb261f3597824e16fe97ffd1b3f
Ethan Chen <intervigil@gmail.com>
2018-03-01 21:31:15 -0800

    power: Use log/log.h instead of utils/Log.h
    Change-Id: I827c7f5ab27548eec8d6e4ee27a72fa883025002

--- Android.mk
+++ Android.mk
@@ -8,7 +8,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libbase libutils android.hardware.power-ndk_platform libbinder_ndk
-LOCAL_HEADER_LIBRARIES += libutils_headers
 LOCAL_HEADER_LIBRARIES += libhardware_headers
 LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c Power.cpp main.cpp
 
