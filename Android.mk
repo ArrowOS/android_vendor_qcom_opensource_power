35add2446558e655c2813e81005cc736b08e49e1
Michael Bestas <mkbestas@lineageos.org>
2018-06-09 16:02:34 +0300

    power: Remove powerhintparser
    * The usage of this code was removed in commit
      0c226b7921b421dd24363f15aefef1236aa25287
    * powerhint.xml is now read by libqti-perfd
    
    Change-Id: I498ad19d0bb43124e4518c5a4f20191a2c122997

--- Android.mk
+++ Android.mk
@@ -7,12 +7,10 @@ ifeq ($(call is-vendor-board-platform,QCOM),true)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libhidlbase libhidltransport libutils android.hardware.power@1.2
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libbase libhidlbase libhidltransport libutils android.hardware.power@1.2
 LOCAL_HEADER_LIBRARIES += libutils_headers
 LOCAL_HEADER_LIBRARIES += libhardware_headers
-LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c service.cpp Power.cpp
-LOCAL_C_INCLUDES := external/libxml2/include \
-                    external/icu/icu4c/source/common
+LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c service.cpp Power.cpp
 
 # Include target-specific files.
 ifeq ($(call is-board-platform-in-list, msm8974), true)
