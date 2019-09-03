99b75f8193ef42b643d6ea6ddd03b4c38d5e0b85
Sumit Gemini <sgemini@codeaurora.org>
2019-09-03 13:34:00 +0530

    power: Add config and makefiles
    Added product makefiles. Moved powerhint XMLs to this project.
    
    Change-Id: Icd0d1e46d0d9793197eb2e5fb5b65c06a5858647

--- power-vendor-product.mk
+++ power-vendor-product.mk
@@ -7,4 +7,6 @@ ifeq ($(TARGET_BOARD_PLATFORM),msmnile)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/msmnile/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 else ifeq ($(TARGET_BOARD_PLATFORM),kona)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/kona/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+else ifeq ($(TARGET_BOARD_PLATFORM),lito)
+PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/lito/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 endif
