f27684995a75fdf234a3e8cee9723c0d1f03d0da
Michael Bestas <mkbestas@lineageos.org>
2019-09-29 23:59:05 +0300

    power: clang-format
    * Using AOSP interface .clang-format
    * Clean Android.mk while we are at it
    
    Change-Id: I630f72e3dffb676ca1930e72945e897f62103ada

--- power-common.h
+++ power-common.h
@@ -51,21 +51,16 @@ extern "C" {
 
 #include <hardware/power.h>
 
-enum CPU_GOV_CHECK {
-    CPU0 = 0,
-    CPU1 = 1,
-    CPU2 = 2,
-    CPU3 = 3
-};
+enum CPU_GOV_CHECK { CPU0 = 0, CPU1 = 1, CPU2 = 2, CPU3 = 3 };
 
 void power_init(void);
-void power_hint(power_hint_t hint, void *data);
+void power_hint(power_hint_t hint, void* data);
 void set_interactive(int on);
 
-#define UNUSED(x) UNUSED_ ## x __attribute__((__unused__))
+#define UNUSED(x) UNUSED_##x __attribute__((__unused__))
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif //__POWER_COMMON_H___
+#endif  //__POWER_COMMON_H___
