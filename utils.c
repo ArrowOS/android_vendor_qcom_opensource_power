5fed76e5dd44eba9d43526045afd7c7c5a49e209
Michael Bestas <mkbestas@lineageos.org>
2019-09-30 23:04:35 +0300

    power: Introduce is_schedutil_governor
    Change-Id: Ie3cfd0cb11b368cd9399a6dcea6b6cfa1f4d7916

--- utils.c
+++ utils.c
@@ -192,6 +192,11 @@ int is_interactive_governor(char* governor) {
     return 0;
 }
 
+int is_schedutil_governor(char* governor) {
+    if (strncmp(governor, SCHEDUTIL_GOVERNOR, (strlen(SCHEDUTIL_GOVERNOR) + 1)) == 0) return 1;
+    return 0;
+}
+
 #ifndef INTERACTION_BOOST
 void interaction(int duration, int num_args, int opt_list[]) {
 #else
