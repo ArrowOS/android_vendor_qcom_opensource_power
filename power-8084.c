823475c9e0a7705e0cf8a4a73848f13a9535431a
Michael Bestas <mkbestas@lineageos.org>
2019-10-06 02:05:22 +0300

    power: Clean up and fix video encode/decode hint handling
    * Unify code style for non legacy SoCs
    * Restore legacy SoC handling which used to live in power-common.c
      but was removed in commit 561cffcbfa66192e839e040d2da3bff2ecf8dcac
    
    Change-Id: Ieb84e6d6c8d762614e21267e81e6057aa64b36dd

--- power-8084.c
+++ power-8084.c
@@ -49,6 +49,97 @@
 
 static int first_display_off_hint;
 
+static int process_video_encode_hint(void* metadata) {
+    char governor[80];
+    struct video_encode_metadata_t video_encode_metadata;
+
+    if (!metadata) return HINT_NONE;
+
+    if (get_scaling_governor(governor, sizeof(governor)) == -1) {
+        ALOGE("Can't obtain scaling governor.");
+        return HINT_NONE;
+    }
+
+    /* Initialize encode metadata struct fields */
+    memset(&video_encode_metadata, 0, sizeof(struct video_encode_metadata_t));
+    video_encode_metadata.state = -1;
+    video_encode_metadata.hint_id = DEFAULT_VIDEO_ENCODE_HINT_ID;
+
+    if (parse_video_encode_metadata((char*)metadata, &video_encode_metadata) == -1) {
+        ALOGE("Error occurred while parsing metadata.");
+        return HINT_NONE;
+    }
+
+    if (video_encode_metadata.state == 1) {
+        if (is_interactive_governor(governor)) {
+            int resource_values[] = {TR_MS_30, HISPEED_LOAD_90, HS_FREQ_1026,
+                                     THREAD_MIGRATION_SYNC_OFF, INTERACTIVE_IO_BUSY_OFF};
+            perform_hint_action(video_encode_metadata.hint_id, resource_values,
+                                ARRAY_SIZE(resource_values));
+            return HINT_HANDLED;
+        }
+    } else if (video_encode_metadata.state == 0) {
+        if (is_interactive_governor(governor)) {
+            undo_hint_action(video_encode_metadata.hint_id);
+            return HINT_HANDLED;
+        }
+    }
+    return HINT_NONE;
+}
+
+static int process_video_decode_hint(void* metadata) {
+    char governor[80];
+    struct video_decode_metadata_t video_decode_metadata;
+
+    if (!metadata) return HINT_NONE;
+
+    if (get_scaling_governor(governor, sizeof(governor)) == -1) {
+        ALOGE("Can't obtain scaling governor.");
+        return HINT_NONE;
+    }
+
+    /* Initialize decode metadata struct fields */
+    memset(&video_decode_metadata, 0, sizeof(struct video_decode_metadata_t));
+    video_decode_metadata.state = -1;
+    video_decode_metadata.hint_id = DEFAULT_VIDEO_DECODE_HINT_ID;
+
+    if (parse_video_decode_metadata((char*)metadata, &video_decode_metadata) == -1) {
+        ALOGE("Error occurred while parsing metadata.");
+        return HINT_NONE;
+    }
+
+    if (video_decode_metadata.state == 1) {
+        if (is_interactive_governor(governor)) {
+            int resource_values[] = {TR_MS_30, HISPEED_LOAD_90, HS_FREQ_1026,
+                                     THREAD_MIGRATION_SYNC_OFF};
+            perform_hint_action(video_decode_metadata.hint_id, resource_values,
+                                ARRAY_SIZE(resource_values));
+            return HINT_HANDLED;
+        }
+    } else if (video_decode_metadata.state == 0) {
+        if (is_interactive_governor(governor)) {
+            undo_hint_action(video_decode_metadata.hint_id);
+            return HINT_HANDLED;
+        }
+    }
+    return HINT_NONE;
+}
+
+int power_hint_override(power_hint_t hint, void* data) {
+    int ret_val = HINT_NONE;
+    switch (hint) {
+        case POWER_HINT_VIDEO_ENCODE:
+            ret_val = process_video_encode_hint(data);
+            break;
+        case POWER_HINT_VIDEO_DECODE:
+            ret_val = process_video_decode_hint(data);
+            break;
+        default:
+            break;
+    }
+    return ret_val;
+}
+
 int set_interactive_override(int on) {
     char governor[80];
 
