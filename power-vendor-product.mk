69bd5a4d31371f83980f33273185284b9852335f
Sowjanya Galla <ssowjany@codeaurora.org>
2020-02-18 17:28:55 +0530

    Add Powerhal support for bengal
    Enable camera powerhint
    
    Change-Id: I00758b785cbfd375e2bce7dbfa27ed03047c0914

--- power-vendor-product.mk
+++ power-vendor-product.mk
@@ -11,4 +11,6 @@ else ifeq ($(TARGET_BOARD_PLATFORM),lito)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/lito/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 else ifeq ($(TARGET_BOARD_PLATFORM),atoll)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/atoll/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+else ifeq ($(TARGET_BOARD_PLATFORM),bengal)
+PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/bengal/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 endif
