33ccecb3e4e7ade3438744d2f1cbeccc9b2ad81d
Shayak Biswas <shaybisw@codeaurora.org>
2020-07-26 10:40:08 +0530

    power : Modified make file to include Talos
    Addition of powerhint.xml for the product Talos
    
    Change-Id: I65c86aa5e3af8ee7d10266d0cb3a46b0a8cf8229

--- power-vendor-product.mk
+++ power-vendor-product.mk
@@ -11,4 +11,6 @@ else ifeq ($(TARGET_BOARD_PLATFORM),lito)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/lito/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 else ifeq ($(TARGET_BOARD_PLATFORM),atoll)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/atoll/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+else ifeq ($(TARGET_BOARD_PLATFORM),sm6150)
+PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/sm6150/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 endif
