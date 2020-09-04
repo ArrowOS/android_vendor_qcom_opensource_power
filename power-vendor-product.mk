a3038ec9312b1b2034f14c89466e59b0a21a5209
KANCHERL SAI YASHWANTH REDDY <kancherl@codeaurora.org>
2020-09-04 17:19:17 +0530

    power : adding power hal for holi
    Added powerhint.xml for holi and changed make file
    
    Change-Id: Ibbc5908caec37719d766616326579ae3576eada0

--- power-vendor-product.mk
+++ power-vendor-product.mk
@@ -13,4 +13,6 @@ else ifeq ($(TARGET_BOARD_PLATFORM),atoll)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/atoll/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 else ifeq ($(TARGET_BOARD_PLATFORM),lahaina)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+else ifeq ($(TARGET_BOARD_PLATFORM),holi)
+PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/holi/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 endif
