c13939d63a09fa301f53c8c9fc106d0e1c312e91
StyloGey <stylogey@gmail.com>
2021-12-31 17:15:58 +0100

    power: drop overrides for sdm660 on 4.19
    * The whole thing is only used if the interactive sched is used.
      Since 4.19 uses EAS, the only allowed scheduler is schedutil,
      therefore it makes no sense to keep it.
    
    Change-Id: Idf91743f0b7c6e4bb8bb1e4e7974eff8c3ad8707

--- Android.mk
+++ Android.mk
@@ -91,7 +91,9 @@ LOCAL_SRC_FILES += power-8998.c
 endif
 
 ifeq ($(call is-board-platform-in-list,sdm660), true)
+ifneq ($(TARGET_KERNEL_VERSION), 4.19)
 LOCAL_SRC_FILES += power-660.c
+endif
 endif
 
 ifeq ($(call is-board-platform-in-list,sdm845), true)
